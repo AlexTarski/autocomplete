520ff3258f87dfe372d03463b136d20ea9898dea
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-03-16 00:08:16 +0300

    Autocomplete implemented

--- AutocompleteTask.cs
+++ AutocompleteTask.cs
@@ -30,8 +30,25 @@ internal class AutocompleteTask
 	/// <remarks>Эта функция должна работать за O(log(n) + count)</remarks>
 	public static string[] GetTopByPrefix(IReadOnlyList<string> phrases, string prefix, int count)
 	{
-		// тут стоит использовать написанный ранее класс LeftBorderTask
-		return null;
+        // тут стоит использовать написанный ранее класс LeftBorderTask
+        if (phrases.Count == 0) return Array.Empty<string>();
+        int index = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) + 1;
+        if (index >= phrases.Count) return Array.Empty<string>();
+        List<string> result = new();
+        while (count > 0 && index < phrases.Count)
+        {
+            if (phrases[index].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
+            {
+                result.Add(phrases[index]);
+                count--;
+                index++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return result.ToArray();
 	}
 
 	/// <returns>
@@ -40,7 +57,7 @@ internal class AutocompleteTask
 	public static int GetCountByPrefix(IReadOnlyList<string> phrases, string prefix)
 	{
 		// тут стоит использовать написанные ранее классы LeftBorderTask и RightBorderTask
-		return -1;
+		return RightBorderTask.GetRightBorderIndex(phrases, prefix, -1, phrases.Count) - LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) - 1;
 	}
 }
 
