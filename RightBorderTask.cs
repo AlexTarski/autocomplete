180f0834ac773fb4b679dab3532a18dc74c62e57
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-03-15 22:52:41 +0300

    right border search implemented

--- RightBorderTask.cs
+++ RightBorderTask.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,25 +8,37 @@ namespace Autocomplete;
 
 public class RightBorderTask
 {
-	/// <returns>
-	/// Возвращает индекс правой границы. 
-	/// То есть индекс минимального элемента, который не начинается с prefix и большего prefix.
-	/// Если такого нет, то возвращает items.Length
-	/// </returns>
-	/// <remarks>
-	/// Функция должна быть НЕ рекурсивной
-	/// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
-	/// </remarks>
-	public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+    /// <returns>
+    /// Возвращает индекс правой границы. 
+    /// То есть индекс минимального элемента, который не начинается с prefix и большего prefix.
+    /// Если такого нет, то возвращает items.Length
+    /// </returns>
+    /// <remarks>
+    /// Функция должна быть НЕ рекурсивной
+    /// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
+    /// </remarks>
+    /// 
+
+    public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
 	{
 		// IReadOnlyList похож на List, но у него нет методов модификации списка.
 		// Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
-		for (int i = phrases.Count-1; i >= 0; i--)
-		{
-			if (string.Compare(prefix, phrases[i], StringComparison.InvariantCultureIgnoreCase) >= 0 
-			    || phrases[i].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
-				return i + 1;
-		}
-		return 0;
-	}
+		
+        while (left <= right)
+        {
+            if (left + 1 == right) return right;
+            var m = left + (right - left) / 2;
+
+            if (string.Compare(prefix, phrases[m], StringComparison.InvariantCultureIgnoreCase) <= 0)
+            {
+                right = m - 1;
+            }
+            else
+            {
+                left = m + 1;
+            }
+        }
+
+        return phrases.Count;
+    }
 }
